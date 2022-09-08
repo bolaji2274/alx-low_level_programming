b53475ab191822568df92478cfbd17a6a17d179f
Bolaji Hammed <hammedbolajihammed@gmail.com>
2022-09-08 16:50:01 -0700

    file modified

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,7 +9,7 @@ int main(void)
 	printf("Size of a char: %ld bytes(s)\n", (long unsigned int)sizeof(char));
 	printf("Size of an int: %ld bytes(s)\n", (long unsigned int)sizeof(int));
 	printf("Size of a long int: %ld bytes(s)", (long unsigned int)sizeof(long int));
-	print("Size of a long long int: %ld bytes(s)\n", (long unsigned int)sizeof(long long int));
+	printf("Size of a long long int: %ld bytes(s)\n", (long unsigned int)sizeof(long long int));
 	printf("Size of a float: %ld bytes(s)\n", (long unsigned int)sizeof(float));
 	return (0);
 }
