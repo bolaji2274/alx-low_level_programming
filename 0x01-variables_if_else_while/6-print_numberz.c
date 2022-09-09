6e2d696d6035ea76e3b292490aec2b184672b68e
Bolaji Hammed <hammedbolajihammed@gmail.com>
2022-09-09 14:03:35 -0700

    putchar print base 10 number

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	int i = 48;
+
+	while (i < 58)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
