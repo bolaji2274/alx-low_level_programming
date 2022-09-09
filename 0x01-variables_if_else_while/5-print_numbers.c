2136f6376d787fd4ab40cb7449e2910073dfd3bc
Bolaji Hammed <hammedbolajihammed@gmail.com>
2022-09-09 13:30:47 -0700

    file added

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		printf("%d\n", i);
+		i++;
+	}
+	return (0);
+}
+
