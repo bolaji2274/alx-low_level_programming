1c1d135b13ce42b7aca9bfb6e3f042bceb88a5de
Bolaji Hammed <hammedbolajihammed@gmail.com>
2022-09-09 16:49:45 -0700

    File 9 added

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+		if (n != 57)
+		{
+			putchar(',');
+			putchar('');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
