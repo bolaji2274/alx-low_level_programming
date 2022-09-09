b38e766f13f75486f53f1f05f9d96c392b0ec624
Bolaji Hammed <hammedbolajihammed@gmail.com>
2022-09-09 15:01:42 -0700

    file

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Succes)
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
