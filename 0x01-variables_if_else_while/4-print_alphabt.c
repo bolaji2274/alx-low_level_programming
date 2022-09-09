11bf69ee4042126cfff83046982a617f38a901e8
Bolaji Hammed <hammedbolajihammed@gmail.com>
2022-09-09 12:09:33 -0700

    alphabet without p and q added

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch != 'p' && ch != 'q')
+		{
+			putchar(ch);
+		}
+		putchar('\n');
+	}
+	return (0);
+}
