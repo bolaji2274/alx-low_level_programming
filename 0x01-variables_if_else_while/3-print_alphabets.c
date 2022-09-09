7a71af7c806d26293f308894d389fa2e9da379a5
Bolaji Hammed <hammedbolajihammed@gmail.com>
2022-09-09 10:18:50 -0700

    File added

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+#include <stdio.h>
+int main(void)
+{
+	char ch;
+	char ck;
+
+	for (ch = 'a'; ch <= 'z'; c++)
+	{
+		putchar(ch);
+	}
+	for (ck = 'A'; ck <= 'Z'; ck++)
+	{
+		putchar(ck);
+	}
+	putchar('\n');
+	return (0);
+}
