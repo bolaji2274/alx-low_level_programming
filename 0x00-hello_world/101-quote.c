681a1f072642f3888da4e1cba31a6ba9f54745c5
Bolaji Hammed <hammedbolajihammed@gmail.com>
2022-09-08 18:28:06 -0700

    file stderr added

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+/**
+ * * main - Entry point
+ * *
+ * * Return: Always 0 (Success)
+ */
+#include <stdio.h>
+#include <unistd.h>
+
+int main(void)
+{
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
