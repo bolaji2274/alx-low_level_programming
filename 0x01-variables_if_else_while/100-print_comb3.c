ff0f93500663993181dc178818e7f88af8cabb22
Bolaji Hammed <hammedbolajihammed@gmail.com>
2022-09-09 21:03:40 -0700

    possible different combinations of two digits.

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	int n, m;
+
+	for (n = 48; n <= 57; n++)
+	{
+		for (m = 48; m <= 57; m++)
+		{
+			putchar(n);
+			putchar(m);
+			if (n != 57 || m != 57)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
