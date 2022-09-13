77522ebaca47bdfe1fc1ecce3155d1fc45d43113
Bolaji Hammed <hammedbolajihammed@gmail.com>
2022-09-13 16:51:41 -0700

    print alphabet ten times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *print_alphabet_x10 - print 10 times the alphabet
+ */
+
+void print_alphabet_x10(void)
+{
+	char m;
+	int i;
+
+	for (m = 'a'; m <= 'z'; m++)
+	{
+		for (i = 0; i < 10; i++)
+		{
+			_putchar(m);
+		}
+		_putchar('\n');
+	}
+}
