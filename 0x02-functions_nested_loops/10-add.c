d39cfc3a24b7bdf0f38e099a65a1995ed18a2725
Bolaji Hammed <hammedbolajihammed@gmail.com>
2022-09-13 19:47:15 -0700

    program to sum two number n and m

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - adds two a and b and returns the result
+ * @n: int to be added to b
+ * @m: int to be added to a
+ * Return: sum of a and b
+ */
+int add(int n, int m)
+{
+	int k;
+
+	k = n + m;
+
+	return (k);
+}
