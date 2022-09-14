64f6a94bdcd1e0ad99a3f48d5d81bd676ef1d1d5
Bolaji Hammed <hammedbolajihammed@gmail.com>
2022-09-13 19:33:59 -0700

    file last_print_digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,7 +2,7 @@
 
 /**
  * print_last_digit - prints the last digit of a number
- * @k: the int to extract the last 
+ * @k: the int to extract the last
  * Return: value of the last digit
  */
 int print_last_digit(int k)
