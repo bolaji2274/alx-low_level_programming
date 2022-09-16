19c2cc9e2f93d653ea169b486736b4934cc01f7e
Bolaji Hammed <hammedbolajihammed@gmail.com>
2022-09-15 17:52:18 -0700

    comment a bug out

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
-/** * main - causes an infinite loop * Return: 0 */ 
+/** * main - causes an infinite loop * Return: 0 	
+* unable to iterate
+*/ 
 int main(void) {
         int i; printf("Infinite loop incoming :(\n"); 
         i = 0; while (i < 10) {
-                putchar(i); //No iteration for the code
+                putchar(i); 
         }
         printf("Infinite loop avoided! \\o/\n"); 
         return (0);
