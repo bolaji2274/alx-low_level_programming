e0bb92c7a40b8fa0d00e433d115c6a00b4625edd
Bolaji Hammed <hammedbolajihammed@gmail.com>
2022-09-16 21:42:51 -0700

    file 8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_square - Function for print the square
+ * @size: int size
+ *Return: 0 Always (Success)
+ */
+
+void print_square(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i, j;
+
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#);
+			}
+			_putchar('\n');
+		}
+	}
+}
