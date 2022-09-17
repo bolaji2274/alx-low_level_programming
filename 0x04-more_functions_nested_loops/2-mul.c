0bf502c1bee064d0ebcba1166f5411673d45262d
Bolaji Hammed <hammedbolajihammed@gmail.com>
2022-09-16 20:03:36 -0700

    file multiplaction added

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * mul - multiply two number
+ * @a: int to be multiplied by b
+ * @b: int to be multiplied to a
+ *
+ * Return: 0 Always 
+ */
+
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+
+	return c;
+}
+
