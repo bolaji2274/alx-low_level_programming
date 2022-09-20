4cf4e2f64eadd76bf7d34d4590479c85d61a4c75
Bolaji Hammed <hammedbolajihammed@gmail.com>
2022-09-20 14:03:31 -0700

    function that convert a string to an integer.

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdio.h>
+#include <string.h>
+/**
+ * _atoi - This function return an interger for a string value
+ * @s: This is an argument for the function
+ * Return: This will return the new value
+ */
+
+int _atoi(char *s)
+{
+	int len;
+
+	len = atoi(s);
+
+	return (len);
+}
