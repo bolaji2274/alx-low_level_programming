f2e52d731f232b730cd6b38a2e258624cc3f39d4
Bolaji Hammed <hammedbolajihammed@gmail.com>
2022-09-22 02:32:28 -0700

    function to capitalise

---
+++ 0x06-pointers_arrays_strings/6main.c
@@ -0,0 +1,51 @@
+#include "main.h"
+#include <string.h>
+/**
+ * cap_string - This function capitalise all world
+ * @str: The string to capitalise
+ * Return: This function return str
+ */
+char *cap_string(char *str)
+{
+	int i, j;
+	char space[13];
+
+	space[14] = {' ', '\t', '\n', ',', ';', ':', '!', '?',
+'"', '(', ')', '{', '}'};
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i == 0 && str[i] >= 'a' && str[i] <= 'z')
+			str[i] = str[i] - 32;
+
+		for (j = 0; j < 13; j++)
+		{
+			if (str[i] == space[j])
+			{
+				if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+				{
+					str[i + 1] -= 32;
+				}
+			}
+		}
+	}
+
+	return (str);
+}
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char str[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\nhello world! hello-world 0123456hello world\thello world.hello world\n";
+    char *ptr;
+
+    ptr = cap_string(str);
+    printf("%s", ptr);
+    printf("%s", str);
+    return (0);
+}
