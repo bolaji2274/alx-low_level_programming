d6546f961baee2472a8735e945c98059aa3bd378
Bolaji Hammed <hammedbolajihammed@gmail.com>
2022-09-21 20:17:30 -0700

    function to capitalise

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * cap_string - This function capitalise all world
+ * @str: The string to capitalise
+ * Return: This function return str
+ */
+char *cap_string(char *str)
+{
+	int i, j;
+	char space[13];
+
+	space[13] = {'', '\t', '\n', ',', ';', ':', '!', '?',
+		'"', '(', ')', '{', '}'};
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i == 0 && str[i] >= 'a' && str[i] <= 'z')
+			str[i] = str[i] - 32;
+
+		for (j = 0; j < 13; j++)
+		{
+			if (str[i] == space[j])
+			{
+				if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+				{
+					str[i + 1] -= 32;
+				}
+			}
+		}
+	}
+
+	return (src);
+}
