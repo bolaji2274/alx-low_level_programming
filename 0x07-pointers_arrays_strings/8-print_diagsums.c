ef9785a4be5a9fa1dc5ade6b7bd901c560b2f8ca
Bolaji Hammed <hammedbolajihammed@gmail.com>
2022-09-27 05:37:01 -0700

    This function print the sum diagonal of the matrix

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -8,7 +8,7 @@
 
 void print_diagsums(int *a, int size)
 {
-	int i, j, sum1, sum2;
+	unsigned int i, j, sum1, sum2;
 
 	sum1 = 0;
 	sum2 = 0;
@@ -18,7 +18,7 @@ void print_diagsums(int *a, int size)
 		for (j = 0; j < size; j++)
 		{
 			if (i == j)
-				sum1 += a[i][i];
+				sum1 += a[i][j];
 		}
 	}
 	for (i = 0, i < size; i++)
@@ -26,7 +26,7 @@ void print_diagsums(int *a, int size)
 		for (j = 0; j < size; j++)
 		{
 			if ((i + j) == (size - 1))
-				sum2 += a[i][i];
+				sum2 += a[i][j];
 		}
 	}
 	printf("%d, %d\n", sum1, sum2);
