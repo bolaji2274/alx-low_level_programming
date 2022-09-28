83d88d7e657ff1f93ea3f11bf73f04cfd0cf833c
Bolaji Hammed <hammedbolajihammed@gmail.com>
2022-09-28 12:08:02 -0700

    This function return the power of a function

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _pow_recursion -This function return the power of x to y
+ * @x: This is main variable
+ * @y: This is passing variable for y
+ * Return: Return the power of y to value x
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	return (x * _pow_recursion(x, y - 1));
+}
