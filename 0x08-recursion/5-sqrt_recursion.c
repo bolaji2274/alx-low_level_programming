4968b38f68a4bb7f0bb686eba06d1809b0b0187f
Bolaji Hammed <hammedbolajihammed@gmail.com>
2022-09-28 14:40:33 -0700

    This function find the square root of a number

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * _sqrt_recursion - This function return the square root of n
+ * @n: This is an interger variable for the function
+ * Return: Return the square root of n
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	return (check_sqrt(n, 0));
+}
+
+/**
+ * check_sqrt -This function the the root of number
+ * @n: Number to find the root
+ * @i: iterator
+ * Return: return the square root
+ *
+ */
+
+int check_sqrt(int n, int i)
+{
+	if (i * i > n)
+		return (-i);
+	if (i * i == n)
+		return (i);
+	return (check_sqrt(n, i + 1));
+}
